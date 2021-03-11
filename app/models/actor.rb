f1de0e793183539071e3341ad422fe14b11dec1d
nathan hains <nathanhains14@yahoo.com>
2021-03-10 21:10:21 -0500

    commit tree ea2bf95039531679cc9f6cd180b1da0bcb8fa1b5

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    full_name = []
+    full_name << self.first_name
+    full_name << self.last_name
+    full_name.join(" ")
+  end
+
+  def list_roles
+    role = []
+    role << self.characters[0].name
+    role << self.shows[0].name
+    Array(role.join(" - "))
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :character
+  belongs_to :network
+
+  def actors_list
+    Array(self.characters[0].actor.full_name)
+  end
+
 end
